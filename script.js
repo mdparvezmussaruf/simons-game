d46386d72db6fa26ff79840ac06f9b9d82e0b2eb
MD Parvez Mussaruf <49514187+MD-Parvez-Mussaruf@users.noreply.github.com>
2025-01-10 20:06:26 +0600

    script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ $(".btn").on("click", function () {
     let userChosenColour = $(this).attr("id");                   // Store the id of the clicked button
     userClickedPattern.push(userChosenColour);                    //user input pattern store to "userClickedPattern"
     playSound(userClickedPattern);  
+    animatePress(userClickedPattern); 
 });
 function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);             //random Number Generator 0-3 (4)
@@ -15,12 +16,23 @@ function nextSequence() {
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100); //Fade button
     
-    playSound();
+    playSound(randomChosenColour);
 }
-function playSound(randomChosenColour){
-    let audio = new Audio("./sounds/" + randomChosenColour + ".mp3");
+function playSound(name){
+    let audio = new Audio("./sounds/" + name + ".mp3");                //audio location
     audio.play();                                                      //audio play
 }
+
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");                          //add button animation class from css
+    setTimeout(function () {$("#" + currentColour).removeClass("pressed")}, 100); //remove button animation class
+}
+$(document).keydown(function() {   
+    if (!window.keyPressed) {                                      // Check if the function has been called before
+      window.keyPressed = true;                                    // Set the flag to true after the first press
+      nextSequence();                                              // Call your function here
+    }});
+  
 
 
 
